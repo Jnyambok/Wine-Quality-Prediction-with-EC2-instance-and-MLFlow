80bc3125232860f42155e9aafaa29bc82d7bada1
Jnyambok <juliusnyambok14@gmail.com>
2024-12-09 21:59:38 +0300

    Added evaluation module

--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -1,7 +1,8 @@
 from mlProject.constants import *
 from mlProject.utils.common import read_yaml, create_directories
 from mlProject.entity.config_entity import (DataIngestionConfig,
-                                            DataValidationConfig,DataTransformationConfig,ModelTrainerConfig)
+                                            DataValidationConfig,
+                                            ModelEvaluationConfig,DataTransformationConfig,ModelTrainerConfig)
 
 
 
@@ -86,3 +87,23 @@ class ConfigurationManager:
         )
 
         return model_trainer_config
+    
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
+        config = self.config.model_evaluation
+        params = self.params.ElasticNet
+        schema =  self.schema.TARGET_COLUMN
+
+        create_directories([config.root_dir])
+
+        model_evaluation_config = ModelEvaluationConfig(
+            root_dir=config.root_dir,
+            test_data_path=config.test_data_path,
+            model_path = config.model_path,
+            all_params=params,
+            metric_file_name = config.metric_file_name,
+            target_column = schema.name,
+            mlflow_uri="https://dagshub.com/Jnyambok/Wine-Quality-Prediction-with-EC2-instance-and-MLFlow.mlflow",
+           
+        )
+
+        return model_evaluation_config
